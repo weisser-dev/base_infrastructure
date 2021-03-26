3da90951af067716b1c645c48537516464cfa8b9
Erik Weisser <35608570+whit-e@users.noreply.github.com>
2021-03-26 23:11:47 +0100

    replace cause i tried it to much....

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(whit-e.com)
+domains=(weisser.dev)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="erikweisser@gmail.com" # Adding a valid address is strongly recommended
